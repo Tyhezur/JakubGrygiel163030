d7e12c161fd0ac50b8a55c24300e828bac127a1e
Tyhezur <jakub77.grygiel@gmail.com>
2024-07-02 00:10:59 +0200

    Adding some new functions

--- File3.java
+++ File3.java
@@ -2,4 +2,5 @@ package studia.JakubGrygiel163030;
 
 public class File3 {
     int a =1;
+    int two = 2;
 }
--- File4.java
+++ File4.java
@@ -2,4 +2,5 @@ package studia.JakubGrygiel163030;
 
 public class File4 {
     int b = 4;
+    int c = 5;
 }
